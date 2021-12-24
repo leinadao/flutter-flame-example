fcb3dac126546a40783beb6e79290686503c0a78
Daniel Osborne <danaosb@gmail.com>
2021-12-24 16:34:36 +0000

    Add in world.

--- lib/game.dart
+++ lib/game.dart
@@ -1,12 +1,15 @@
 import 'package:flame/game.dart';
 import '../helpers/direction.dart';
 import 'components/player.dart';
+import 'components/world.dart';
 
 class FFEGame extends FlameGame {
   final Player _player = Player();
+  final World _world = World();
 
   @override
   Future<void> onLoad() async {
+    await add(_world);
     add(_player);
   }
 
