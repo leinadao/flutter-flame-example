5e20422c736c496e2726407990e454be1ab64ca0
Daniel Osborne <danaosb@gmail.com>
2021-12-24 13:12:06 +0000

    Add player.

--- lib/game.dart
+++ lib/game.dart
@@ -1,8 +1,11 @@
 import 'package:flame/game.dart';
+import 'components/player.dart';
 
 class FFEGame extends FlameGame {
+  final Player _player = Player();
+
   @override
   Future<void> onLoad() async {
-    // empty
+    add(_player);
   }
 }
